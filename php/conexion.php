042259d26162d7a4296fb71c5db3136d236efa08
Daniel Gaspar Saldarriaga <daniel.saldarriaga@da.arus.com.co>
2017-03-13 10:28:55 -0500

    -a para cosas modificadas

---
+++ php/conexion.php
@@ -0,0 +1,17 @@
+<?php
+
+	$db_host="";
+	$db_usuario="";
+	$db_contraseña="";
+	$db_nombre="";
+
+	$conexion=mysqli_connect($db_host,$db_usuario,$db_contraseña,$db_nombre);
+	if (mysqli_connect_errno()) 
+	{
+		echo "ERROR AL CONECTAR CON LA BASE DE DATOS, POR FAVOR VUELVE A INTENTAR MAS TARDE O CONTACTESE CON EL ADMINISTRADOR";
+		exit();	
+	}
+
+	mysqli_set_charset($conexion,"utf8");
+
+?>
